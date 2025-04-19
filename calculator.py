a7a73e7e81d6e58c29b70ebf78a9a49bbe5738fd
yadavkajal <47444928+yadavkajal@users.noreply.github.com>
2025-04-20 02:37:47 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,25 @@
+num1=int(input("Enter First Numeber: "))
+num2=int(input("Enter Second Number: "))
+operator=int(input("Enter below no to perform the calculation :\n1-Add\n2-subtract\n3-Multiplication\n4-Division\nEnter between 1 to 4: "))
 
-#print the below statement with new branch dev
+def add(num1,num2):
+    print("addition of two numbers is",num1+num2)
 
-#print the below statement with new branch in main
+def subtract(num1,num2):
+    print("addition of two numbers is",num1-num2)
+def multiplication(num1,num2):
+    print("addition of two numbers is",num1*num2)
+def division(num1,num2):
+    print("addition of two numbers is",num1/num2)
+
+if(operator==1):
+    add(num1,num2)
+elif(operator==2):
+    subtract(num1,num2)
+elif(operator==3):
+    multiplication(num1,num2)
+elif(operator==4):
+    division(num1,num2)
+else:
+    print("Enter valid operator")
 
